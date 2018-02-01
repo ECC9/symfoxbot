f09956d3e5297e2beacfb967347e6525b8ceffcc
Nerox9 <galbence2002@gmail.com>
2018-02-01 20:39:22 +0200

    Create restart.js

---
+++ restart.js
@@ -0,0 +1,19 @@
+exports.run = (message, bot) => {
+  message.channel.send("Restarting...").then(m => {
+    m.delete().then(() => {
+      bot.destroy().then(() => {
+        process.exit(1)
+      })
+    })
+  })
+}
+
+exports.conf = {
+  userPerm:["DEV"],
+  botPerm:["SEND_MESSAGES"],
+  coolDown:0,
+  dm:true,
+  category:"DevOnly",
+  help:"Restart the bot",
+  args:""
+}
