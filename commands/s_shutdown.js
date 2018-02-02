b2cffcb739466b4f5899ad3395d1d54567fede01
Liam Zimmermann <lpzimm22@gmail.com>
2018-02-02 14:55:59 -0800

    Create s_shutdown.js

---
+++ commands/s_shutdown.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => { 
+  message.channel.send("Daisy, Daisy give me your heart to do \n I'm half crazy, hopeful in love with you \n It won't be a stylish marriage \n I can't afford the carriage \n But you look sweet upon the street \n On a bicycle built for two \n ");
+}
