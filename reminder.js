431614734124b01abaeb901182a783f849d10da3
Nerox9 <galbence2002@gmail.com>
2018-02-02 18:10:12 +0200

    Update reminder.js

--- reminder.js
+++ reminder.js
@@ -1,5 +1,11 @@
 while (true) {
   console.log("true is never false, so I will never stop!");
 }
-
 //I have to try this code for fun.
+
+//A new one lol.
+var x = 0;
+
+while (x < 1) {
+  console.log('Oops! x is never incremented from 0, so i will ALWAYS be less than 1');
+}
