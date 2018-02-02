84d9b20a0b40da06d4be4c77e715901669b0162a
Liam Zimmermann <lpzimm22@gmail.com>
2018-02-02 14:19:02 -0800

    added b and m commands

---
+++ commands/b_thx.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => { 
+  message.channel.send("No problem! I'm happy to help you. :smile:");
+}
--- commands/hi.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-  message.channel.send(`Hello there! :D How are you doing? ${message.author}`);
-}
