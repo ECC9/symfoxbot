22b6875d2c826f1df80ca5177c4bcb10c05c86f9
Nerox9 <galbence2002@gmail.com>
2018-02-03 19:22:01 +0200

    Update m_cookie.js

--- commands/m_cookie.js
+++ commands/m_cookie.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => { 
-  message.channel.send("I gave you a cookie! :cookie: Do you need milk with it? :milk: ");
+  message.channel.send("I gave you a :cookie:! Do you need :milk: with it?");
 }
