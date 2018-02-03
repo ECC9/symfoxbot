6ab2a7866e2acbbf42c16d84031f9c89f90e58e2
Nerox9 <galbence2002@gmail.com>
2018-02-03 19:21:10 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args, Discord, RichEmbed) => {
       color: 3447003,
       fields: [{
           name: "Symfox's Info\n",
-          value: "Version: `1.0.0 beta`\nCreated by `ECC9#6057`, `Gaiben#7736`, `lpz#3218`, `Anarchy#8213`, `[-TM-] TechyMan#0594` and `ViperChan#9895`.\n\nEmail: `galbence2002@gmail.com`"
+          value: "Version: `1.0.0 `\nCreated by `ECC9#6057`, `Gaiben#7736`, `lpz#3218`, `Anarchy#8213`, `[-TM-] TechyMan#0594` and `ViperChan#9895`.\n\nEmail: `galbence2002@gmail.com`"
       }
       ],
       timestamp: new Date(),
