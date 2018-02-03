3358c541c9dcd27fecad4adc5259ab70912ab898
Nerox9 <galbence2002@gmail.com>
2018-02-03 19:19:42 +0200

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => { 
-  message.channel.send(`Welcome back, ${message.author}! We missed you! :smile:`);
+  message.channel.send(`Welcome back, I removed your AFK, ${message.author}! We missed you! :smile:`);
   message.member.removeRole("353620798197727234");
 }
