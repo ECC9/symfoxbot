1b36b194ec5f1339b6648e6427fbd62444d464f7
Nerox9 <galbence2002@gmail.com>
2018-02-03 19:18:08 +0200

    Update b_hi.js

--- commands/b_hi.js
+++ commands/b_hi.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-  message.channel.send(`Hello there! :D How are you doing? ${message.author}`);
+  message.channel.send(`Hello there! :D How are you doing?`);
 }
