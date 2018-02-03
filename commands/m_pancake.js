37a4bf3e1336518653afedc91d75a720e84e9eba
Nerox9 <galbence2002@gmail.com>
2018-02-03 19:23:10 +0200

    Update m_pancake.js

--- commands/m_pancake.js
+++ commands/m_pancake.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => { 
-    message.channel.send("Here are some pancakes! :pancakes:", {files: [{attachment: 'http://www.seriouseats.com/images/2015/05/20150517-pancake-primary.jpg' }]} );
+    message.channel.send("Here are some :pancake: !", {files: [{attachment: 'http://www.seriouseats.com/images/2015/05/20150517-pancake-primary.jpg' }]} );
 }
