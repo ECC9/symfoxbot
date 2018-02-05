64a86600786f695b9ddd6892cdffa308b59dbac7
Nerox9 <galbence2002@gmail.com>
2018-02-05 22:47:38 +0200

    Add files via upload

---
+++ commands/time.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, args) => { 
+  message.channel.send("So, you want to run huh?");
+  message.channel.send("Say something fast");
+  message.channel.send("After that, check your time right now.");
+  message.channel.send("Remember that time.");
+  message.channel.send("Now as you can see you spent a lot of time reading this shit.");
+  message.channel.send("Thanks for playing the time game,");
+  message.channel.send("now you learned how to spend your time with adult content :laughing:");
+  message.channel.send("Have fun next time :p :wink:");
+  message.channel.send("Cya.");
+}
