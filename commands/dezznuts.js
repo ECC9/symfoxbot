18e17e817c38d5ce3f4bccb1b7d636d1b8f993d3
Nerox9 <galbence2002@gmail.com>
2018-02-06 22:26:58 +0200

    Update dezznuts.js

--- commands/dezznuts.js
+++ commands/dezznuts.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => { 
-       message.channel.send("DEEZNUTS", {files: [{attachment: 'https://i.ytimg.com/vi/5LlQNty_C8s/hqdefault.jpg' }]} ); 
+       message.channel.send("HAH! GOT IIIIT", {files: [{attachment: 'https://i.ytimg.com/vi/5LlQNty_C8s/hqdefault.jpg' }]} ); 
 }
