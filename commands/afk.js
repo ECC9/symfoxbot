002fa44285d8b486edba9f4391eb84de764a01e3
Nerox9 <galbence2002@gmail.com>
2018-07-20 16:19:06 +0300

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => { 
-  message.channel.send("${message.author} , I set you AFK.");
+  message.channel.send(`${message.author} , I set you AFK.`);
   message.member.addRole("role_id");
 }
