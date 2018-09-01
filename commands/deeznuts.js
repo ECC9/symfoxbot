9bdfc3c07e941c502850168960eee1cc3035d8f2
Nerox9 <galbence2002@gmail.com>
2018-09-01 10:13:49 -0700

    Update deeznuts.js

--- commands/deeznuts.js
+++ commands/deeznuts.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => { 
-       message.channel.send("HAH! GOT IIIIT", {files: [{attachment: 'https://i.ytimg.com/vi/5LlQNty_C8s/hqdefault.jpg' }]} ); 
+       message.channel.send("HAH! GOT IIIIT", {files: [{attachment: 'https://media.giphy.com/media/CYU3D3bQnlLIk/giphy.gif' }]} ); 
 }
